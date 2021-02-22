15f22c8e8323f5163839f88e2ac3941e05ff48b3
jobaile <joannabaile@gmail.com>
2021-02-22 18:09:00 -0500

    understanding setState

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+class App extends React.Component{
+    //A function that is special to the JS language
+    constructor(props){
+        super(props);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+        //number default is null
+        //this is the only time we do direct assignment 
+        //to this.state
+        //everytime we want to change or manipulate a property, use setState
+        this.state = { lat: null, errorMessage: '' };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                //we called setState
+                //coords and latitude was found inside the development in chrome
+                this.setState({ lat: position.coords.latitude });
+
+                //we did not write:
+                //this.state.lat = position.coords.latitude
+            },
+            err => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
+    }
+
+    componentDidMount(){
+        console.log('My component was rendered to the screen');
+    }
+
+    componentDidUpdate(){
+        console.log('My component was just updated - it rerendered');
+    }
+
+    //React says we have to define render
+    render() {
+        if (this.state.errorMessage && !this.state.lat){
+            return <div> Error: {this.state.errorMessage} </div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat} </div>
+        }
+
+        //catchall!
+        return <div>Loading!</div>;
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
